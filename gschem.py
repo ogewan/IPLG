e5642169f253ae2f2bab932ec9d8b3c173f51836
Seun Ogedengbe <seun40@gmail.com>
2015-02-22 16:58:23 -0600

    changing format to .schematic
    no longer using minecraft to render
    using MCEDIT to render, .schematic file is a lot simpler

---
+++ gschem.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+"""
+Create a file .schematic
+"""
+import os,sys
+import time
+import random
+from io import BytesIO
+from struct import pack, unpack
+import array, math
+# local module
+try:
+    import nbt
+except ImportError:
+    # nbt not in search path. Let's see if it can be found in the parent folder
+    extrasearchpath = os.path.realpath(os.path.join(__file__,os.pardir,os.pardir))
+    if not os.path.exists(os.path.join(extrasearchpath,'nbt')):
+        raise
+    sys.path.append(extrasearchpath)
+from nbt.nbt import NBTFile, TAG_Short, TAG_Int, TAG_String, TAG_List, TAG_Byte, TAG_Byte_Array, TAG_Compound
+from nbt.chunk import Chunk
+
+class foo():
+    id = 10
+
+def _schema(h=256,l=128,w=128,blst=[0]*4194304):
+    result = NBTFile() #Blank NBT
+    result.name = "Schematic"
+    result.tags.extend([
+        TAG_Short(name="Height", value=h),
+        TAG_Short(name="Length", value=l),
+        TAG_Short(name="Width", value=w),
+        TAG_Byte_Array("Biomes",gbba([0]*16384,True)),
+        TAG_Byte_Array("Blocks", gbba(blst,True)),
+        TAG_Byte_Array("Data",gbba([0]*4194304,True)),
+        TAG_String(name="Materials", value="Alpha"),
+        TAG_List(type=foo(),name="Entities"),
+        TAG_List(type=foo(),name="TileEntities"),
+        TAG_List(type=foo(),name="TileTicks")
+    ])
+    return result
+
+
+def gbba(blocksList, buffer=False):
+    """Return a list of all blocks in this chunk."""
+    if buffer:
+        length = len(blocksList)
+        return BytesIO(pack(">i", length)+gbba(blocksList))
+    else:
+        return array.array('B', blocksList).tostring()
+
+if __name__ == '__main__':
+    schmatic = _schema()
+    print(schmatic.pretty_tree())
+    schmatic.write_file("artificial2.schematic")
